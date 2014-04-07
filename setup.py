1c35270adfbbedfb4d60a92046897ae2ba0454d7
Antti Kervinen <antti.kervinen@intel.com>
2014-04-07 16:17:44 +0300

    started packaging and enabled Sphinx-based docs

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name         = 'opentestrobot',
+    version      = '0.0',
+    description  = 'Robot APIs for platform independent automated testing',
+    author       = 'Antti Kervinen',
+    author_email = 'antti.kervinen@intel.com',
+    packages     = ['opentestrobot'],
+    package_data = {'opentestrobot': ['__init__.py',
+                                      'gestureapi.py',
+                                      'swemulation.py',
+                                      'userinteractionapi.py']})
